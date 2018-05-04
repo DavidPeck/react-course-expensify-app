6e304dc13e3fe0ea5b0251d1ec9ac8afff3eab48
Peck <D2Px@pge.com>
2018-05-03 17:28:34 -0700

    setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ return {
     entry: './src/app.js',
     //entry: './src/playground/hoc.js',
     output: {
-        path:  path.join(__dirname ,'public'),
+        path:  path.join(__dirname ,'public', 'dist'),
         filename: 'bundle.js'
     }, 
     module : {
@@ -50,7 +50,9 @@ return {
     devtool: isProduction ? 'source-map' :  'inline-source-map',
     devServer:  {
         contentBase: path.join(__dirname ,'public'),
-        historyApiFallback: true
+        historyApiFallback: true,
+        publicPath: '/dist/'
+
     },
     performance: {
         hints: false
